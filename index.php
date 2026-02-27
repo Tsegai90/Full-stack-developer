f2365065764c6f490f3efac6bc7d8cb61cc9e1b3
redae <erias451@gmail.com>
2026-02-27 14:23:58 +0100

    Add database config constants and connection script

--- index.php
+++ index.php
@@ -1,37 +1,38 @@
-<!DOCTYPE html>
-<html>
-    <body>
+<html lang="en">
+<body>
 
+<?php
 
-        <?php
-// parent class
-class fruit {
-    public $name;
-    public $color;
-
-    public function __construct($name, $color) 
-    {
-        $this -> name =$name;
-        $this ->color=$color; 
-    }
-     public function intro () 
-     {
-        echo "The fruit is {$this->name} and its color is {$this->color} .<br>"; 
-     }
+// Configuration class with constants
+class Config {
+    const DB_HOST = "localhost";        // MySQL server
+    const DB_USER = "myuser";           // Your MySQL username
+    const DB_PASS = "StrongPass123!";   // Your MySQL password
+    const DB_NAME = "my_database";      // Your database name
 }
-// strawberry is inherited from fruit
-class strawberry extends fruit
- {
-    public function message ()
-    {
-        echo " am i a fruit or berry";
-    }
-}
-  $strawberry = new strawberry("strawberry", "red");
-  $strawberry -> intro();
-  $strawberry ->message();
 
-  
-    ?>
-    </body>
+// Create a new MySQLi connection using constants
+$mysqli = new mysqli(Config::DB_HOST, Config::DB_USER, Config::DB_PASS, Config::DB_NAME);
+
+// Check connection
+if ($mysqli->connect_error) {
+    die("Database connection failed: " . $mysqli->connect_error);
+}
+
+// Success message
+echo "Connected successfully to database '" . Config::DB_NAME . "'";
+
+// Example query
+$result = $mysqli->query("SELECT NOW() AS new_time");
+
+if ($result) {
+    $row = $result->fetch_assoc();
+    echo "<br>Current database time: " . $row['new_time'];
+} else {
+    echo "<br>Error executing query: " . $mysqli->error;
+}
+
+?>
+
+</body>
 </html>
