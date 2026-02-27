017dc69e046756497fc19debc98b108f6bf9f505
redae <erias451@gmail.com>
2026-02-27 11:38:37 +0100

    Added PHP OOP Fruit project

--- index.php
+++ index.php
@@ -1,25 +1,37 @@
-<?php 
-include("index.html");
-$query = htmlspecialchars($_GET['query'], ENT_QUOTES, 'UTF-8');
-echo "You searched for: " . $query;
-// Check if the 'query' parameter exists and is not empty
-if (isset($_GET['query']) && !empty($_GET['query'])) {
-    $query = htmlspecialchars($_GET['query']);  // Sanitize the input
-    echo "Du har søgt efter: " . $query;  // Display the search term
-} else {
-    echo "Indtast venligst et søgeord.";  // Message if no query parameter is provided
-}
-?>
-<?php 
-include("index.html");
-$query = htmlspecialchars($_GET['query'], ENT_QUOTES, 'UTF-8');
-echo "You searched for: " . $query;
-// Check if the 'query' parameter exists and is not empty
-if (isset($_GET['query']) && !empty($_GET['query'])) {
-    $query = htmlspecialchars($_GET['query']);  // Sanitize the input
-    echo "Du har søgt efter: " . $query;  // Display the search term
-} else {
-    echo "Indtast venligst et søgeord.";  // Message if no query parameter is provided
-}
-?>
+<!DOCTYPE html>
+<html>
+    <body>
 
+
+        <?php
+// parent class
+class fruit {
+    public $name;
+    public $color;
+
+    public function __construct($name, $color) 
+    {
+        $this -> name =$name;
+        $this ->color=$color; 
+    }
+     public function intro () 
+     {
+        echo "The fruit is {$this->name} and its color is {$this->color} .<br>"; 
+     }
+}
+// strawberry is inherited from fruit
+class strawberry extends fruit
+ {
+    public function message ()
+    {
+        echo " am i a fruit or berry";
+    }
+}
+  $strawberry = new strawberry("strawberry", "red");
+  $strawberry -> intro();
+  $strawberry ->message();
+
+  
+    ?>
+    </body>
+</html>
